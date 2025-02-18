9bf286d85aed304784123a0e8ec37fd4e53e50f7
Simon <simon.kwong64@gmail.com>
2025-02-17 20:53:22 -0800

    use json instead of postgres for database since it's overkill for this scale

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js';
-import { bind_user } from './db';
+import { load_db, save_db } from './db';
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+var db = await load_db('./db.json');
 
 client.on(Events.ClientReady, readyClient => {
   console.log(`Logged in as ${readyClient.user.tag}`)
@@ -11,18 +12,16 @@ client.on(Events.InteractionCreate, async interaction => {
   if (!interaction.isChatInputCommand()) return;
 
   if (interaction.commandName === 'ping') {
-    await bind_user(interaction.user.id, interaction.guild?.id, interaction.channel?.id);
-    console.log(interaction.user.id, interaction.channelId);
-    console.log(interaction.guild?.id);
     await interaction.reply('pong')
   }
 
   if (interaction.commandName === 'bind') {
-
-    // When you init the bot should bind a users "saved" messages to the current channel
-    await bind_user(interaction.user.id, interaction.guild?.id, interaction.channel?.id);
-    console.log(interaction.user.id, interaction.channelId);
-    await interaction.reply('Successfully binded to channel');
+    if (!db.has(interaction.user.id)) {
+      db.set(interaction.user.id, interaction.channelId);
+    }
+    db.set(interaction.user.id, interaction.channelId);
+    await save_db('./db.json', db);
+    await interaction.reply(`Successfully bound to ${interaction.channelId}`);
   }
 
 })
@@ -39,7 +38,6 @@ client.on(Events.MessageCreate, async message => {
   await message.forward(channel);
   if (message.reference && message.mentions.users.has(client.user?.id)) {
     console.log(ref_msg.attachments)
-    // console.log(ref_msg_content);
 
   }
 })
